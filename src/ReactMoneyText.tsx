226df570e81a22839429ed30db917624fad29b1f
JuanDArdilaG <39010502+JuanDArdilaG@users.noreply.github.com>
2025-01-04 17:46:17 -0500

    fix: pass classes to childer component

--- src/ReactMoneyText.tsx
+++ src/ReactMoneyText.tsx
@@ -2,10 +2,17 @@ import { PriceValueObject } from "@juandardilag/value-objects/PriceValueObject";
 
 export const ReactMoneyText = ({
   value,
+  className,
   CustomComponent,
 }: {
   value: PriceValueObject;
+  className?: string;
   CustomComponent?: any;
 }) => {
-  return <CustomComponent children={<p>{value.toString()}</p>} />;
+  return (
+    <CustomComponent
+      className={className}
+      children={<p>{value.toString()}</p>}
+    />
+  );
 };
